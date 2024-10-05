cf2d0357f62eeef148f3e26e4d31d1d212d98065
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-10-05 13:52:46 +0400

    initial commit

---
+++ PaceX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaceX
+//
+//  Created by Temur Chitashvili on 05.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaceX/PaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceXApp.swift
+//  PaceX
+//
+//  Created by Temur Chitashvili on 05.10.24.
+//
+
+import SwiftUI
+
+@main
+struct PaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
